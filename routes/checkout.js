88b03e54d91a94303188c24fe92bf7953fa87f9a
Sun-Deep <sundeepp29@gmail.com>
2020-01-13 20:02:23 +0545

    minor changes in billing part

--- routes/checkout.js
+++ routes/checkout.js
@@ -13,10 +13,29 @@ router.get('/checkout', (req, res) => {
 
 router.get('/bill/:book_id',  (req, res) => {
     let final_bill = []
-    con.query("SELECT DATE_FORMAT(hotel_booking.checked_in, '%Y-%m-%d') AS checked_in, DATE_FORMAT(hotel_booking.checked_out, '%Y-%m-%d') AS checked_out, hotel_room.room_id, hotel_categories.name, hotel_cat_rate.rate, hotel_cat_rate.extra_bed, hotel_booking.booked_by, hotel_cat_rate.extra_guest FROM hotel_booking JOIN hotel_room ON hotel_room.room_id = hotel_booking.room_id AND hotel_booking.book_id = ? JOIN hotel_cat_rate ON hotel_room.cat_id = hotel_cat_rate.cat_id JOIN hotel_categories ON hotel_cat_rate.cat_id = hotel_categories.id",
+    con.query("SELECT DATE_FORMAT(hotel_booking.checked_in, '%Y-%m-%d') AS checked_in, DATE_FORMAT(hotel_booking.checked_out, '%Y-%m-%d') AS checked_out, hotel_room.room_id, hotel_categories.name, hotel_cat_rate.rate, hotel_cat_rate.extra_bed as extra_bed_rate, hotel_booking.booked_by, hotel_cat_rate.extra_guest, hotel_booking.adult, hotel_booking.extra_bed FROM hotel_booking JOIN hotel_room ON hotel_room.room_id = hotel_booking.room_id AND hotel_booking.book_id = ? JOIN hotel_cat_rate ON hotel_room.cat_id = hotel_cat_rate.cat_id JOIN hotel_categories ON hotel_cat_rate.cat_id = hotel_categories.id",
     [req.params.book_id], (error, results, fields) => {
-        console.log(results)
-        res.json(results)
+        final_bill.push(
+            {person_info: [results[0].booked_by, results[0].checked_in, results[0].checked_out]}
+        )
+        for(let i = 0; i < Object.keys(results).length; i++){
+            console.log(results[i].adult)
+            let temp_amount = results[i].rate + results[i].extra_guest * Math.abs(results[i].adult - 1) + results[i].extra_bed * results[i].extra_bed_rate
+            console.log(temp_amount)
+            final_bill.push({
+                room_id: results[i].room_id,
+                room_cat: results[i].name,
+                amount: results[i].rate,
+                extra_bed_rate: results[i].extra_bed_rate,
+                extra_guest: results[i].extra_guest,
+                adult: results[i].adult,
+                extra_bed: results[i].extra_bed,
+                total_amount: temp_amount,
+
+            })
+        }
+        console.log(final_bill)
+        res.json(final_bill)
     })
     
 })
